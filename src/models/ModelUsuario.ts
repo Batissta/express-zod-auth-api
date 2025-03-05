32008dc00fd73ce0a1efd3aee0d21fe8f71ed064
Batissta <francinaldofilho05@gmail.com>
2025-03-05 15:17:32 -0300

    Feat - implementando o model de usuarios

---
+++ src/models/ModelUsuario.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+type TUsuario = {
+  id: string;
+  nome: string;
+  email: string;
+  senha: string;
+  tipo: "passageiro" | "motorista";
+};
+
+const userSchema = new mongoose.Schema<TUsuario>({
+  id: { type: String, required: true, unique: true },
+  nome: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  senha: { type: String, required: true },
+  tipo: { type: String, default: "passageiro", required: true },
+});
+
+export default mongoose.model<TUsuario>("usuarios", userSchema);
