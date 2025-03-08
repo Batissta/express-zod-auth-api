80bfb37b1b62125478c48f9294f8b198840e8833
Batissta <francinaldofilho05@gmail.com>
2025-03-08 00:46:21 -0300

    Feat - Implementando o model de motoristas -> Modelo da collection no mongodb

--- src/models/ModelMotorista.ts
+++ src/models/ModelMotorista.ts
@@ -1,15 +1,31 @@
 import mongoose from "mongoose";
 
-export type TMotorista = {
-  usuarioId: string;
-  viagens: [string];
-  avaliacoes: [string];
+export type TVeiculo = {
+  marca: string;
+  modelo: string;
+  ano: number;
+  placa: string;
+  cor: string;
 };
 
-const userSchema = new mongoose.Schema<TMotorista>({
-  usuarioId: { type: String, ref: "usuarios", required: true, unique: true },
-  viagens: { type: [String], required: true, default: [] },
-  avaliacoes: { type: [String], required: true, default: [] },
+export type TMotorista = {
+  usuarioId: string;
+  veiculo: TVeiculo;
+  autenticado: boolean;
+};
+
+const veiculoSchema = new mongoose.Schema<TVeiculo>({
+  ano: { type: Number, required: true },
+  cor: { type: String, required: true },
+  marca: { type: String, required: true },
+  modelo: { type: String, required: true },
+  placa: { type: String, required: true, unique: true },
 });
 
-export default mongoose.model<TMotorista>("motoristas", userSchema);
+const motoristaSchema = new mongoose.Schema<TMotorista>({
+  usuarioId: { type: String, ref: "usuarios", required: true, unique: true },
+  veiculo: { type: veiculoSchema, required: true },
+  autenticado: { type: Boolean, required: true, default: false },
+});
+
+export default mongoose.model<TMotorista>("motoristas", motoristaSchema);
