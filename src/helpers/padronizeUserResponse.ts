aa2ba73e5e432c38d91ca4ae81a9e0cc51bc4ea4
Batissta <francinaldofilho05@gmail.com>
2025-03-08 00:43:23 -0300

    Feat - implementando funções para padronizar as responses para o cliente final

--- src/helpers/padronizeUserResponse.ts
+++ src/helpers/padronizeUserResponse.ts
@@ -1,14 +1,5 @@
 import { z } from "zod";
-
-export type TUserUnpadronized = {
-  id: string;
-  nome: string;
-  email: string;
-  senha: string;
-  tipo: string;
-  viagensId: [string];
-  avaliacoesId: [string];
-};
+import { veiculoSchema } from "../schema/MotoristaZod";
 
 const schemaUserUnpadronized = z.object({
   id: z.string(),
@@ -19,6 +10,7 @@ const schemaUserUnpadronized = z.object({
   viagensId: z.array(z.string()),
   avaliacoesId: z.array(z.string()),
 });
+export type TSchemaUserUnpadronized = z.infer<typeof schemaUserUnpadronized>;
 
 const schemaUserPadronized = schemaUserUnpadronized.transform((data) => ({
   nome: data.nome,
@@ -27,14 +19,72 @@ const schemaUserPadronized = schemaUserUnpadronized.transform((data) => ({
   viagensId: data.viagensId,
   avaliacoesId: data.avaliacoesId,
 }));
+3;
 
-export const padronizaResponseUsers = (payload: TUserUnpadronized[]) => {
-  const usuariosTransformados = payload.map((user: any) => {
+export const padronizaResponseUsers = (payload: TSchemaUserUnpadronized[]) => {
+  const usuariosTransformados = payload.map((user) => {
     return schemaUserPadronized.safeParse(user).data;
   });
   return usuariosTransformados;
 };
 
-export const padronizaResponseUser = (payload: TUserUnpadronized) => {
-  return schemaUserPadronized.safeParse(payload);
+export const padronizaResponseUser = (payload: TSchemaUserUnpadronized) => {
+  const result = schemaUserPadronized.safeParse(payload);
+  if (!result.success)
+    return {
+      errors: result.error.errors.map((e) => e.message),
+      ...result,
+    };
+  return result;
+};
+
+const esquemaMotoristaNaoPadronizado = z.object({
+  usuario: z.array(
+    z.object({
+      id: z.string(),
+      nome: z.string(),
+      email: z.string(),
+      tipo: z.string(),
+      viagensId: z.array(z.string()),
+      avaliacoesId: z.array(z.string()),
+    })
+  ),
+  autenticado: z.boolean(),
+  veiculo: veiculoSchema,
+});
+export type TypeMotoristaNaoPadronizado = z.infer<
+  typeof esquemaMotoristaNaoPadronizado
+>;
+
+const esquemaMotoristaPadronizado = esquemaMotoristaNaoPadronizado.transform(
+  (data) => ({
+    id: data.usuario[0].id,
+    nome: data.usuario[0].nome,
+    email: data.usuario[0].email,
+    viagensId: data.usuario[0].viagensId,
+    avaliacoesId: data.usuario[0].avaliacoesId,
+    autenticado: data.autenticado,
+    veiculo: data.veiculo,
+  })
+);
+
+export const padronizeMotoristasResponse = (
+  payload: TypeMotoristaNaoPadronizado[]
+) => {
+  const usuariosTransformados = payload.map((user) => {
+    return esquemaMotoristaPadronizado.safeParse(user).data;
+  });
+  return usuariosTransformados;
+};
+
+export const padronizeMotoristaResponse = (
+  payload: TypeMotoristaNaoPadronizado
+) => {
+  const result = schemaUserPadronized.safeParse(payload);
+  if (!result.success)
+    return {
+      erros: result.error.errors.map((e) => e.message),
+      ...result,
+    };
+  return result;
 };
