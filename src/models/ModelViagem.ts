16b27fded04e22f0c4bdb7d308cdac66d5d00eef
Batissta <francinaldofilho05@gmail.com>
2025-03-12 11:18:31 -0300

    Feat - Criando o model de viagem

---
+++ src/models/ModelViagem.ts
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+export type TViagem = {
+  id: string;
+  motoristaId: string;
+  passageirosId: [string];
+  data: Date;
+  hora: {
+    horas: number;
+    minutos: number;
+  };
+  origem: string;
+  destino: string;
+};
+
+const viagemSchema = new mongoose.Schema({
+  id: { type: String, required: true },
+  motoristaId: { type: String, required: true },
+  passageirosId: { type: [String], default: [] },
+  data: { type: Date, required: true },
+  hora: {
+    horas: { type: Number, required: true },
+    minutos: { type: Number, required: true },
+  },
+  origem: { type: String, required: true },
+  destino: { type: String, required: true },
+});
