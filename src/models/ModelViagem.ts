4c963b7342da5db3e0b2afac06eb5b7c80ff7aff
Batissta <francinaldofilho05@gmail.com>
2025-03-13 11:44:22 -0300

    Refactor - Adicionando relacionamento 1:1 com o modelo de usuários

--- src/models/ModelViagem.ts
+++ src/models/ModelViagem.ts
@@ -11,12 +11,13 @@ export type TViagem = {
   };
   origem: string;
   destino: string;
+  status: "confirmada" | "concluida" | "pendente";
 };
 
 const viagemSchema = new mongoose.Schema<TViagem>({
   id: { type: String, required: true, unique: true },
-  motoristaId: { type: String, ref: "motoristas", required: true },
-  passageirosId: { type: [String], default: [] },
+  motoristaId: { type: String, ref: "motoristas", default: "" },
+  passageirosId: { type: [String], ref: "usuarios", default: [] },
   data: { type: String, required: true },
   hora: {
     horas: { type: Number, required: true },
@@ -24,6 +25,7 @@ const viagemSchema = new mongoose.Schema<TViagem>({
   },
   origem: { type: String, required: true },
   destino: { type: String, required: true },
+  status: { type: String, default: "pendente" },
 });
 
 export default mongoose.model<TViagem>("viagens", viagemSchema);
