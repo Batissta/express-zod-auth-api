ad46f6524077a37a122ad028bb936139276178cb
Batissta <francinaldofilho05@gmail.com>
2025-03-13 11:41:23 -0300

    Feat - Implementando o apollo-server ao express

--- src/server.ts
+++ src/server.ts
@@ -4,28 +4,49 @@ import mongoose from "mongoose";
 import env from "./config/config";
 import routeUsuarios from "./routes/routeUsuario";
 import routeViagens from "./routes/routeViagem";
+import { typeDefs } from "./schemas/viagemTypeDefs";
+import { resolvers } from "./resolvers/resolvers";
+import { ApolloServer } from "apollo-server-express";
 
-const app = express();
+const app: express.Application | any = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/usuarios", routeUsuarios);
 app.use("/api/viagens", routeViagens);
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
 mongoose.connect(env.DB_STRING_CONNECTION).then(() => {
   console.log("🎈 Mongodb database initialized!");
   app.emit("DATABASE_CONNECTED");
 });
 
-app.on("DATABASE_CONNECTED", () => {
-  app.listen(env.PORT, () => {
-    console.log(`🚀 HTTP Server is running!`);
+const startServer = async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+  console.log("GraphQL is running!");
+  // app.listen(env.PORT, () => {
+  //   console.log(
+  //     `🚀 HTTP Server is running!\n📌 Use this API at the follow link: http://localhost:${env.PORT}${server.graphqlPath}`
+  //   );
+  // });
+  app.listen(9999, () => {
     console.log(
-      `📌 Use this API at the follow link: http://localhost:${env.PORT}`
+      `🚀 HTTP Server is running!\n📌 Use this API at the follow link: http://localhost:${9999}${
+        server.graphqlPath
+      }`
     );
   });
+};
+
+app.on("DATABASE_CONNECTED", () => {
+  startServer();
 });
 
-app.get("/", async (_req, res) => {
+app.get("/", async (_req: any, res: any) => {
   res.status(200).json({
     message:
       "Welcome to my API!👋🏽 I'm Francinaldo Batista and you can access the docs of this API at my github! Let's connect at linkedin: https://linkedin.com/in/francinaldobatista",
