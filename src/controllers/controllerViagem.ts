2d6e558d942eb4998081f249281a1d6d500920c4
Batissta <francinaldofilho05@gmail.com>
2025-03-16 19:54:38 -0300

    Feat - createViagem agora utiliza o protocolo GraphQL

--- src/controllers/controllerViagem.ts
+++ src/controllers/controllerViagem.ts
@@ -23,64 +23,45 @@ export const findViagens = async (req: any, res: any) => {
   }
 };
 
-export const createViagem = async (req: any, res: any) => {
+export const createViagem = async (args: any) => {
   try {
-    const result = validateCriarViagem(req.body);
+    const result = validateCriarViagem(args);
     if (!result.success)
-      return res.status(400).json({
-        errors: result.errors,
-      });
+      return {
+        viagem: {},
+        error: "Dados inválidos!",
+        details: result.errors,
+      };
 
     const id = `v.${randomUUID()}`;
-
-    const novaViagem = await viagemRepository.create({
-      id,
-      ...result.data,
-    });
 
     if (result.data.motoristaId) {
       const motorista = await usuarioRepository.findOne({
         id: result.data.motoristaId,
       });
-      if (!motorista?.nome)
-        return res.status(404).json({
-          message: "O motorista não foi encontrado!",
-        });
+
+      if (!motorista?.nome) return {};
       motorista.viagensId.push(id);
       await motorista.save();
     }
-
-    if (!novaViagem.data)
-      throw new Error("Um erro ocorreu na validação dos dados.");
-
-    return res.status(201).json({
-      message: "Viagem criada com sucesso!",
-      viagem: novaViagem,
+    const novaViagem = await viagemRepository.create({
+      id,
+      ...result.data,
     });
-  } catch (error: unknown) {
-    if (error instanceof Error)
-      return res.status(400).json({
-        message: error.message,
-      });
-  }
-};
 
-export const queryFindByMotoristaId = async (motoristaId: string) => {
-  try {
-    const motoristaIsValid = await motoristaRepo.auxQueryFindById(motoristaId);
-    if (!motoristaIsValid) return "ID de motorista inválido!";
-    const motoristaViagens = await viagemRepository.find({ motoristaId });
     return {
-      ...motoristaIsValid,
-      viagens: motoristaViagens,
+      viagem: novaViagem,
+      errors: "",
+      details: [],
     };
   } catch (error: unknown) {
-    if (error instanceof Error) return error.message;
+    if (error instanceof Error)
+      return {
+        viagem: {},
+        errors: error.message,
+        details: [],
+      };
   }
-};
-
-export const findAllViagens = async () => {
-  return await viagemRepository.find();
 };
 
 export const mutationUpdateById = async (id: string, args: any) => {
@@ -107,4 +88,22 @@ export const mutationUpdateById = async (id: string, args: any) => {
     return { error: "Viagem não encontrada!", viagem: null, details: [] };
 
   return { error: "", viagem: updatedViagem, details: [] };
+};
+
+export const queryFindByMotoristaId = async (motoristaId: string) => {
+  try {
+    const motoristaIsValid = await motoristaRepo.auxQueryFindById(motoristaId);
+    if (!motoristaIsValid) return "ID de motorista inválido!";
+    const motoristaViagens = await viagemRepository.find({ motoristaId });
+    return {
+      ...motoristaIsValid,
+      viagens: motoristaViagens,
+    };
+  } catch (error: unknown) {
+    if (error instanceof Error) return error.message;
+  }
+};
+
+export const findAllViagens = async () => {
+  return await viagemRepository.find();
 };
