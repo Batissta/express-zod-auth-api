630fe3e975b537a800a64c13004e26f5e4320e2a
Batissta <francinaldofilho05@gmail.com>
2025-03-17 23:08:59 -0300

    Fix - Corrigindo bugs no middleware: - Ao realizar GET na rota /api/usuarios/ o middleware permitia a passagem, mas a rota deverias ser privada.

--- src/middleware.ts
+++ src/middleware.ts
@@ -9,25 +9,30 @@ const authRoutes = [
 ];
 
 export const middleware = async (req: any, res: any, next: any) => {
-  const IsAprivateRoute = authRoutes.some((authRoutesObject) => {
-    const route = authRoutesObject.route.split(":")[0];
-    return (
-      authRoutesObject.method == req.method &&
-      req.originalUrl.includes(route) &&
-      req.originalUrl.split("/").length > 3 &&
-      !(req.originalUrl.includes("/login") || req.originalUrl.includes("/tipo"))
-    );
-  });
-
-  if (!IsAprivateRoute) return next();
-
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ message: "Formato do token inválido" });
-  }
-
   try {
+    const IsAprivateRoute = authRoutes.some((authRoutesObject) => {
+      const route = authRoutesObject.route.split(":")[0];
+      return (
+        (req.originalUrl === authRoutesObject.route &&
+          req.method === authRoutesObject.method) ||
+        (authRoutesObject.method == req.method &&
+          req.originalUrl.includes(route) &&
+          req.originalUrl.split("/").length > 3 &&
+          !(
+            req.originalUrl.includes("/login") ||
+            req.originalUrl.includes("/tipo")
+          ))
+      );
+    });
+
+    if (!IsAprivateRoute) return next();
+
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      return res.status(401).json({ message: "Formato do token inválido" });
+    }
+
     const token = authHeader.split(" ")[1];
     jwt.verify(token, env.SECRET, (err: unknown, result: any) => {
       if (err && err instanceof Error)
