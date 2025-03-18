55863fcc089fbcf2fb378eea15041d89959b6760
Batissta <francinaldofilho05@gmail.com>
2025-03-17 23:17:57 -0300

    Feat - Mutation deleteById implementada em viagens

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -4,6 +4,7 @@ import {
   queryFindViagemById,
   mutationUpdateById,
   mutationCreateViagem,
+  mutationDeleteById,
 } from "../controllers/controllerViagem";
 
 type paramMotoristaViagens = {
@@ -14,14 +15,21 @@ export const resolvers = {
   Query: {
     motoristaViagens: async (_: any, { motoristaId }: paramMotoristaViagens) =>
       await queryFindByMotoristaId(motoristaId),
+
     findAllViagens: async () => await queryFindAllViagens(),
+
     findViagemById: async (_: any, { id }: { id: string }) =>
       await queryFindViagemById(id),
   },
+
   Mutation: {
     createViagem: async (_: any, { ...args }: any) =>
       await mutationCreateViagem(args),
+
     updateViagemById: async (_: any, { id, ...args }: any) =>
       await mutationUpdateById(id, args),
+
+    deleteViagemById: async (_: any, { id }: { id: string }) =>
+      await mutationDeleteById(id),
   },
 };
